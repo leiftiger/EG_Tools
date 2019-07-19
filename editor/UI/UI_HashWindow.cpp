5fad66c3c2c243bb3daaaf716c3f112584b224a0
leiftiger <leiftiger@users.noreply.github.com>
2019-07-19 20:35:29 +0200

    Added unsigned hash value to hash tool

--- editor/UI/UI_HashWindow.cpp
+++ editor/UI/UI_HashWindow.cpp
@@ -14,5 +14,6 @@ void UI_HashWindow::hashText(const QString &str)
 {
 	std::int32_t hash = util::hashElixir(str.toStdString());
 
-	ui.textHash->setText(QString::fromStdString(std::to_string(hash)));
+	ui.textHashS->setText(QString::fromStdString(std::to_string(hash)));
+	ui.textHashU->setText(QString::fromStdString(std::to_string((std::uint32_t)hash)));
 }
