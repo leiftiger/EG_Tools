df16f6112c28fbb298aae1c07c6211c3399ee5ee
leiftiger <leiftiger@users.noreply.github.com>
2020-09-07 19:36:06 +0200

    Improved build process

--- build.sh
+++ build.sh
@@ -8,12 +8,15 @@ cd editor
 
 cd ../resourcepacker
 make
+echo
 
 cd ../resourceparser
 make
+echo
 
 cd ../interpreter
 make
+echo
 
 cd ../editor
 qmake editor.pro
@@ -23,9 +26,11 @@ mkdir -p GeneratedFiles/Release
 make
 mkdir -p bin
 mv ATN_Editor.out ../x64/Release/
+echo
 
 cd ../resourcemerger
 make
+echo
 
 cd ../modmanager
 qmake modmanager.pro
@@ -35,5 +40,6 @@ mkdir -p GeneratedFiles/Release
 make
 mkdir -p bin
 mv Mod_Manager.out ../x64/Release/
+echo
 
 echo "Successfully compiled all EG tools!"
